c94d50467d49c95ea8c00c4c96f16fadb17dda8f
Hadiqa Rabnawaz <108441711+guleadan@users.noreply.github.com>
2022-07-25 07:07:07 -0400

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Intellisense.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Adan Motors</title>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+        function DisplayOptions() {
+            var inputVal = document.getElementById("carName").value();
+            var num = inputVal.length;
+            $.ajax({
+                url: 'WebForm1.aspx/GenerateList',
+                type: 'post',
+                data: JSON.stringify({ "txt": inputVal, "length": num }),
+                contentType: 'application/json',
+                async: false,
+                success: function (data) {
+                    var inner = document.getElementById("options");
+                    inner.innerHTML = data.d;
+                }
+            });
+
+        }
+        
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <label runat="server"> Welcome to Adan Motors!</label><br />
+            <input type="text" id="carName" placeholder="Enter your desired car name here" onkeypress="DisplayOptions()" />
+            <p runat="server" id="options"></p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Intellisense
+{
+    public partial class Intel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var cars = new List<string>() { "Toyota Avalon", "Toyota Camry", "Toyota Corolla", "Toyota Corolla Hybrid",
+                                             "Honda City", "Honda BR-V", "Honda CR-v", "Honda Civic", "Honda Accord",
+                                             "Audi A4",  "Audi A5", "Audi e-tron GT", "Audi e-tron", "Audi Q7",
+                                             "Mercedes GLA SUV", "Mercedes GLB SUV","Mercedes G-Class SUV", "A-Class Sedan" };
+
+
+        }
+        [WebMethod]
+        public static List<string> GenerateList(string txt, int length)
+        {
+            var cars = new List<string>() { "Toyota Avalon", "Toyota Camry", "Toyota Corolla", "Toyota Corolla Hybrid",
+                                             "Honda City", "Honda BR-V", "Honda CR-v", "Honda Civic", "Honda Accord",
+                                             "Audi A4",  "Audi A5", "Audi e-tron GT", "Audi e-tron", "Audi Q7",
+                                             "Mercedes GLA SUV", "Mercedes GLB SUV","Mercedes G-Class SUV", "A-Class Sedan" };
+            var optList = new List<string>();
+
+            if (length == 1)
+            {
+                for (int j = 0; j < cars.Count; j++)
+                {
+                    if (cars[j][0] == Convert.ToChar(txt))
+                    {
+                        optList.Add(cars[j]);
+                    }
+
+                }
+                optList.Add(Convert.ToString(optList.Count));
+
+
+            }
+            return optList;
+        }
+    }
+}
